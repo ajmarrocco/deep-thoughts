0e512bb4a13aec84cefd38626d7974b71bc37645
ajmarrocco <ajmarrocco@gmail.com>
2022-05-04 21:29:29 -0400

    add auth.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -17,9 +18,19 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
   // instantiates Apollo Client instance and creates the connection to the API endpoint
-  link: httpLink,
+  link: authLink.concat(httpLink),
   // instantiates new cache object
   cache: new InMemoryCache(),
 });
